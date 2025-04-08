0e4640fa3667b32132fa37e4275d8e9c408d9a4d
Avinash Pathak <avipathak732@gmail.com>
2025-04-08 19:06:03 +0530

    Fix CORS Policy Error

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,12 +11,12 @@ const app = express()
 
 const port = process.env.PORT
 
-app.use(express.json())
-app.use(cookieParser())
 app.use(cors({
     origin: "http://localhost:5173",
     credentials: true
 }))
+app.use(express.json({ limit: '10mb' }))
+app.use(cookieParser({ limit: '10mb', extended: true }))
 
 app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
